92c821ebedb926ae6698fff0a6a001c14ad1c34a
Marsel Ayupov <aamarsel99@gmail.com>
2021-04-08 16:31:25 +0300

    Define some new components skeleton

---
+++ app/src/pages/Home/Home.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export interface IHomeProps {}
+
+const Home = (props: IHomeProps) => {
+  return (
+    <div className='centered-content'>
+      <div className='flex-row'>
+      </div>
+    </div>
+  )
+}
+
+export default Home
