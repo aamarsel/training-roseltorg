861ef0a2bfd21b97a69f14cb017a5d8e88215804
Marsel Ayupov <aamarsel99@gmail.com>
2021-04-08 17:11:30 +0300

    Eject the app Add path resolvers Do home page markup

--- app/src/pages/Home/Home.tsx
+++ app/src/pages/Home/Home.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import RoseltorgLogo from 'images/roseltorg-logo.svg'
+import {Button} from "@material-ui/core";
 
 export interface IHomeProps {}
 
 const Home = (props: IHomeProps) => {
   return (
-    <div className='centered-content'>
-      <div className='flex-row'>
+    <div className='fit-container centered-content'>
+      <div className='flex-column'>
+        <img
+          src={RoseltorgLogo}
+          alt='Roseltorg Logo'
+          width={300}
+          height={300}
+        />
+        <Button
+          variant='contained'
+          color='primary'
+        >
+          Войти
+        </Button>
       </div>
     </div>
   )
